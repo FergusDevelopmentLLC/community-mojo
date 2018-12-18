02b7ac273e558cb176efd195d4df1df2f08506d7
Will Carter <will.carter@fergusllc.com>
2018-12-18 11:40:42 -0700

    Added "Will was here"

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default class App extends React.Component {
         <View style={styles.container}>
           <Image source={require('./assets/ReactNativeFirebase.png')} style={[styles.logo]}/>
           <Text style={styles.welcome}>
-            Welcome to {'\n'} React Native Firebase
+            Welcome to -Will was here-{'\n'} React Native Firebase
           </Text>
           <Text style={styles.instructions}>
             To get started, edit App.js
