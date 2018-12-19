54eee0f8b04713300906700f6567c1dbad473dbd
Will Carter <will.carter@fergusllc.com>
2018-12-19 12:26:28 -0700

    added login/signup/logout

---
+++ src/Main.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { StyleSheet, Platform, Image, Text, View, Button } from 'react-native'
+import firebase from 'react-native-firebase'
+
+export default class Main extends React.Component {
+  state = { currentUser: null }
+
+  componentDidMount() {
+    const { currentUser } = firebase.auth()
+
+    this.setState({ currentUser })
+  }
+
+  handleLogout = () => {
+    const { email, password } = this.state
+    firebase
+      .auth()
+      .signOut()
+      .then(() => this.props.navigation.navigate('Login'))
+      .catch(error => this.setState({ errorMessage: error.message }))
+  }
+
+  render() {
+    const { currentUser } = this.state
+
+    return (
+      <View style={styles.container}>
+        <Text>
+          Hi {currentUser && currentUser.email}!
+        </Text>
+        <Button title="Logout" onPress={this.handleLogout} />
+      </View>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
