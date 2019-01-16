108c5509a64c6ed62f062843b377b52081cae9f2
Will Carter <will.carter@fergusllc.com>
2019-01-16 12:44:37 -0700

    members formatting

--- screens/members.js
+++ screens/members.js
@@ -1,12 +1,15 @@
 import React, {Component} from 'react';
 import { StyleSheet, View, Text, TouchableOpacity, Alert, Image } from 'react-native';
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
+import { Icon } from "native-base";
 
 export default class ExampleFour extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      tableHead: ['', 'Name', 'Points', ''],
+      tableHead: [
+        ['', 'Name', 'Points', '']
+      ],
       tableData: [
         ['alien.01.png', 'Thor Cramer', '10', 'X'],
         ['alien.02.png', 'Michelle Oster', '9', 'X'],
@@ -40,37 +43,79 @@ export default class ExampleFour extends Component {
     else if(index === 7)
       return require("./assets/avatars/alien.07.png");
   }
- 
+  
+  renderCellStyle(idx) {
+    switch (idx) {
+      case 1:
+        return {
+          flex: 3
+        }
+      default:
+        return {
+          flex: 1
+        }
+    }
+  }
+
+  renderCellElement(data, rowIndex, columnIndex) {
+    switch (columnIndex) {
+      case 0:
+        return <Image source={this.getSource(rowIndex + 1)} style={{width: 26, height: 34}}></Image>
+      case 3:
+        return <TouchableOpacity onPress={() => this._alertIndex(rowIndex)}>
+                  <View>
+                    <Icon name="ios-eye" />
+                  </View>
+                </TouchableOpacity>
+      default:
+        return <Text>{data}</Text>
+    }
+  }
+
+  renderHead(rowData, rowIndex) {
+    console.log(rowData)
+    console.log(rowIndex)
+  }
+  
+  // {
+  //   state.tableHead.map((rowData, rowIndex) => (
+  //     <Row data={ this.renderHead(rowData, rowIndex) } style={styles.head} textStyle={ styles.text }/>
+  //   ))
+  // }
+
   render() {
     const state = this.state;
-
-    const element = (data, index) => (
-      <TouchableOpacity onPress={() => this._alertIndex(index)}>
-        <View style={styles.btn}>
-          <Text style={styles.btnText}>button</Text>
-        </View>
-      </TouchableOpacity>
-    );
-
-    const image = (data, index) => (
-      <Image source={this.getSource(index + 1)} style={{width: 26, height: 34}} />
-    );
-
+    
     return (
       <View style={styles.container}>
-        <Text>Members</Text>      
+        <Text style={{fontWeight: 'bold', marginBottom: 10}}>Members</Text>      
         <Table borderStyle={{borderColor: 'transparent'}}>
-          <Row data={state.tableHead} style={styles.head} textStyle={styles.text}/>
           {
-            state.tableData.map((rowData, index) => (
-              <TableWrapper key={index} style={styles.row}>
+            state.tableHead.map((rowData, rowIndex) => (
+              <TableWrapper key={rowIndex} style={[styles.row, {height:30}]}>
                 {
                   rowData.map((cellData, cellIndex) => (
-                    // <Cell key={cellIndex} data={cellIndex === 3 ? element(cellData, index) : cellData} textStyle={styles.text}/>
                     <Cell 
-                      key={cellIndex} 
-                      data={cellIndex === 3 ? element(cellData, index) : cellIndex === 0 ? image(cellData, index) : cellData} 
-                      textStyle={cellIndex != 0 ? styles.text : ""} 
+                      key = { cellIndex } 
+                      data = { cellData }
+                      style = {[this.renderCellStyle(cellIndex)]}
+                      textStyle = { cellIndex === 2 ? {textAlign: 'center'} : "" }
+                    />
+                  ))
+                }
+              </TableWrapper>
+            ))
+          }
+          {
+            state.tableData.map((rowData, rowIndex) => (
+              <TableWrapper key={rowIndex} style={styles.row}>
+                {
+                  rowData.map((cellData, cellIndex) => (
+                    <Cell 
+                      key={ cellIndex } 
+                      data={ cellIndex === 2 ? cellData : this.renderCellElement(cellData, rowIndex, cellIndex) }
+                      style={[this.renderCellStyle(cellIndex), { borderTopColor: '#ccc', borderTopWidth: 1 }]}
+                      
                     />
                   ))
                 }
@@ -84,11 +129,40 @@ export default class ExampleFour extends Component {
 }
  
 const styles = StyleSheet.create({
-  container: { flex: 1, padding: 16, paddingTop: 30, backgroundColor: '#fff' },
-  head: { height: 40, backgroundColor: '#fff' },
-  text: { margin: 6 },
-  row: { flexDirection: 'row', backgroundColor: '#fff' },
-  avatar: { width: 26, height: 34 },
-  btn: { width: 58, height: 18, backgroundColor: '#78B7BB',  borderRadius: 2 },
-  btnText: { textAlign: 'center', color: '#fff' }
+  container: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 30,
+    backgroundColor: '#fff'
+  },
+  head: { 
+    height: 30,
+  },
+  text: {
+    margin: 6
+  },
+  row: {
+    flexDirection: 'row',
+    height: 50
+  },
+  btnBox: {
+    flexDirection: 'row'
+  },
+  avatar: { 
+    width: 26,
+    height: 34
+  },
+  btn: { 
+    width: 58, 
+    height: 18, 
+    backgroundColor: '#78B7BB',  
+    borderRadius: 2 },
+  btnText: { 
+    textAlign: 'center',
+    color: '#fff',
+    lineHeight: 26
+  },
+  imgCell: { 
+    lineHeight: 26
+  }
 });
